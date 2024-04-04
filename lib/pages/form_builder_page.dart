4205ce6cf5585fb854b914b7d265595f0806a828
QuantumPickleJar <vmorrilll@gmail.com>
2024-04-04 12:55:57 -0500

    checkpoint-style commit

--- lib/pages/form_builder_page.dart
+++ lib/pages/form_builder_page.dart
@@ -1,6 +1,6 @@
 import 'package:athlete_surveyor/widgets/ExpandedTextField.dart';
 import 'package:flutter/material.dart';
-    
+
 class FormBuilderPage extends StatefulWidget {
   const FormBuilderPage({Key? key}) : super(key: key);
 
@@ -19,28 +19,82 @@ class _FormBuilderPageState extends State<FormBuilderPage> {
         children: [
           ExpandedTextField(labelText: "Title"),
           Divider(),
-          Card(  /// Collapsed question 1
+          Card(
+            /// Collapsed question 1
             margin: EdgeInsets.all(15.0),
-            child: 
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Question 1"),
-                    Text("Expand",),
-                  ]),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [const Text("Open-ended")],
-                  )
+            child: Column(children: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Text("Question 1",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                Text(
+                  "Expand",
+                ),
               ]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [const Text("Open-ended")],
+              )
+            ]),
           ),
           Divider(),
-          Card(       /// Expanded visible question
-          margin: EdgeInsets.all(15.0),
+          Card(
 
-          )
+              /// Expanded visible question
+              margin: EdgeInsets.all(15.0),
+              child: Column(
+                children: [
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Question 2",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                      ]),
+                  ExpandedTextField(labelText: "Prompt"),
+                  Divider(),
+                  Card(
+                    /// attachments card
+                    color: Color.fromRGBO(200, 200, 200, 1.0),
+                    child: Column(children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Attachments", style: TextStyle(fontWeight: FontWeight.bold)),
+                            InputChip(
+                              label: Text("URL"),
+                              isEnabled: true,
+                              avatar: Icon(Icons.language),
+                            ),
+                            InputChip(
+                                label: Text("IMG"),
+                                isEnabled: true,
+                                avatar: Icon(Icons.upload)),
+                          ]),
+                    Divider(color: Colors.white, indent:10, endIndent: 10),
+                      /// replace with listview to show attached files
+                      Column(children: [
+                        Text("(350 x 500) - final_submit.png",
+                        style: TextStyle(color: Colors.red)),
+                        Text("Writing Guidelines - Canvas",
+                        style: TextStyle(color: Colors.red)),
+                      ]),
+                    ]),
+                  )
+                ])
+              ),
+              Divider(),
+              /// Answer Type Selection
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                Text("Format response:", style: TextStyle(fontWeight: FontWeight.bold)),
+                Slider(
+                  onChanged: null,
+                  value: 50
+                )
+                  
+              ], /// end header row
+              )
+              
         ],
       ),
     );
