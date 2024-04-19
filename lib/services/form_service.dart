0d278fbcc4dd98d8374b93f72b89ff3122d39f02
QuantumPickleJar <vince.codefactory@outlook.com>
2024-04-19 10:59:00 -0500

    refactor: added more signatures

--- lib/services/form_service.dart
+++ lib/services/form_service.dart
@@ -1,11 +1,44 @@
 
-
-
 import 'package:athlete_surveyor/models/StudentForm.dart';
 import 'package:athlete_surveyor/models/question.dart';
+import 'package:gsheets/gsheets.dart';
 import 'package:uuid/uuid.dart';
 
 class FormService {
+  final GSheets gsheets;
+  final Spreadsheet spreadsheet;
+  late Worksheet formSheet;
+
+  FormService(this.gsheets, String spreadsheetId) {
+    _init(spreadsheetId);
+  }
+
+  Future<void> _init(String spreadsheetId) async {
+    spreadsheet = await gsheets.spreadsheet(spreadsheetId);
+    formSheet = await _getSheet('Forms');
+  }
+  /// individual sheet within the whole Workfile
+  Future<Worksheet> _getSheet(String title) async {
+    try {
+      return await spreadsheet.addWorksheet(title);
+    } catch (e) {
+      return spreadsheet.worksheetByTitle(title)!;
+    }
+  }
+
+
+  Future<void> writeQuestion(Question question, Uuid, formUuid) async {
+      /// Ex:
+   ///   await formSheet.values.appendRow([
+   ///   formUuid,
+   ///   question.ordinal.toString(),
+   ///   question.header,
+   ///   question.content,
+   ///   question.res_required.toString(),
+   ///   question.resFormat.widgetType.toString(),
+   ///   question.linkedFile?.path ?? ''
+   /// ]);
+  }
 
   /// Called when updates to the form's responses have been made 
   @override 
@@ -21,9 +54,8 @@ class FormService {
     /// 
   }
 
-  /// TODO: verify accuracy of this desc with GPT
   /// using the supplied [formUuid] as an indexing key of sorts, queries the spreadsheet
-  /// accordingly so that the content can be loaded 
+  /// accordingly so that the content can be loaded for a **specific student**
   StudentForm? loadForm(Uuid formUuid) {
     // TODO: implement loadForm
     /// List<Question>? questions = (get from query);
