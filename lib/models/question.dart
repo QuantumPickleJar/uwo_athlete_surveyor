b777f0b5e0f58570c6351e25157c681de012392d
QuantumPickleJar <vince.codefactory@outlook.com>
2024-04-19 15:32:21 -0500

    feat: response-respective question generation

--- lib/models/question.dart
+++ lib/models/question.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
 import 'package:athlete_surveyor/models/response_type.dart';
+import 'package:uuid/uuid.dart';
+
+import 'response.dart';
 
 /// Represents an individual question found on a survey-like form that's 
 /// created/modified by department staff, and doled out to students for 
@@ -7,31 +10,36 @@ import 'package:athlete_surveyor/models/response_type.dart';
 class Question { 
   /// used to control the ordering of the question when loaded on a form
   late int? ordinal;
-  /// TODO: ask GPT/prof if late is appropriate approach for async data
   late String header;   /// TODO: this should default to the ordinal
   late String content;
-  late bool res_required;
+  late bool resRequired;
   
   /// Controls what widget will be shown to students when loaded
-  late ResponseType _resFormat;
+  late ResponseType resFormat;
   
-  /// (Optional) supplements the [content] ideally an IMG, PDF, etc.
-  File? _linkedFile;
+  /// (Optional) supplements the [content], ideally an IMG, PDF, etc.
+  File? linkedFile;
 
-  /// Gets the desired format the response will be
-  ResponseType get resFormat => _resFormat;
+  Question({
+    required this.ordinal, 
+    required this.header,
+    required this.content,
+    required this.resRequired,
+    required this.resFormat,
+    this.linkedFile
+  });
 
-  /// Sets the format of the response
-  set resFormat(ResponseType value) {
-    _resFormat = value;
-  }
+  /// Creates a [Response] by passing [answer] through the [Question] without 
+  /// needing to know what [resFormat]. 
+  Response<dynamic> createResponse(dynamic answer) {
+    // TODO: potentially place validation logic here
 
-  /// If there is a file associated with this question, fetch it
-  File? get linkedFile => _linkedFile;
-
-  /// Sets which file (if any) is attached 
-  set linkedFile(File? value) {
-    _linkedFile = value;
+    return Response<dynamic>(
+      // questionId: TODO
+      questionId: "TEST_ID",
+      answer: answer,
+      responseType: resFormat
+    );
   }
 }
 
