4d8fabef2aec15e8d6f94c3fce4262017e329aad
QuantumPickleJar <vince.codefactory@outlook.com>
2024-04-19 10:50:56 -0500

    build: added gsheet dependency

--- lib/services/forms/draft_service.dart
+++ lib/services/forms/draft_service.dart
@@ -5,13 +5,22 @@ import 'package:uuid/uuid.dart';
 /// form to store the student's responses, but eventually for performance we'll want to change this to
 /// instead only be storing response content *per-[Question]*
 class DraftService {
-  final Worksheet _draftSheet;
+  final Worksheet draftSheet;
   
-  DraftService(this._draftSheet);
+  DraftService(this.draftSheet);
 
   Future<void> saveDraft(Uuid formUuid, String questionId, String draftData) async {
-    // TODO: find the right cell based on formUuid and questionId and save draftData
+    // TODO: TEST THIS 
+    // find the right cell based on formUuid and questionId and save draftData
+    final row = await draftSheet.values.rowByKey(formUuid.toString() + questionId, fromColumn: 1);
     
+    if (row == null) {
+      // Append new draft
+      await draftSheet.values.appendRow([formUuid, questionId, draftData]);
+    } else {
+      // TODO: Update existing draft
+      // await draftSheet.values.insertValue(draftData, column: 3, row: row);
+    }
   }
 
   Future<String> loadDraft(Uuid formUuid, String questionId) async {
