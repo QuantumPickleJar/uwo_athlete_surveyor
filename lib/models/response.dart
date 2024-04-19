f368dbe5a59b7fce4547c3a51f9b5cb355667b11
QuantumPickleJar <vince.codefactory@outlook.com>
2024-04-19 15:22:07 -0500

    feat: init impl of response model

--- lib/models/response.dart
+++ lib/models/response.dart
@@ -1,1 +1,23 @@
 /// Defines a student's response to a Question object.  
+/// Since the form of their response will vary, we call 
+/// it the generic type T.
+///
+import 'response_type.dart';
+
+class Response<T> {
+  /// The Id of the Question that this [Response] is tied to
+  final String questionId;
+
+  /// The student's response that was submitted for a given Question
+  T answer;
+  
+  /// how to represent this answer; set from the [Question]
+  final ResponseType responseType;
+  
+  /// Constructs a new [Response] that uses [T] to store whatever the
+  /// student used to answer the question with id [questionId].
+  Response({ 
+    required this.questionId, 
+    required this.answer,
+    required this.responseType});
+}
