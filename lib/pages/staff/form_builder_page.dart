3ab1ecf3dc99157e8d52a1c3d42ceba6094e707f
QuantumPickleJar <vmorrilll@gmail.com>
2024-04-20 18:19:03 -0500

    apply stash

--- lib/pages/staff/form_builder_page.dart
+++ lib/pages/staff/form_builder_page.dart
@@ -14,52 +14,69 @@ class FormBuilderPage extends StatefulWidget {
 
 
 class _FormBuilderPageState extends State<FormBuilderPage> {
-
   late double _sliderValue;
-  late final _formService;
-  late StaffForm _currentForm;    /// the form currently being modified
+  late final FormService _formService;
+  late StaffForm _currentForm;
+
+  /// the form currently being modified
 
   /// Temporary placeholder to allow the slider to be enabled
   void _onSliderTap() {
     return;
   }
 
-  @override 
+  @override
   void initState() {
     super.initState();
     _formService = Provider.of<FormService>(context, listen: true);
-
+    /// load [_currentForm] via [_formService]
   }
 
   @override
   Widget build(BuildContext context) {
     /// give [_sliderValue] a valid number
     _sliderValue = 0.5;
-
+    /// update [_currentForm]
     return Consumer<StaffForm>(
-      builder: (context, form, child) { 
-        return Scaffold(
-          appBar: AppBar(title: Text('Form Builder - ${form.formName}')),
-          body:  ListView.builder(
-            itemCount: form.questions.length,
-            itemBuilder: (context, index) {
-              Question question = form.questions[index];
-              
-              /// this is where we unpack the contents of the question
-              return ListTile(
-                title: Text(question.header),
-                subtitle: Text(question.content),
-              );
-            }
-          ),
-          
-          
-        );
-      }
+      builder: (context, form, child) {
+        // _currentForm = _formService.;
+
+        return buildQuestions(form);
+      }, 
+      child: Scaffold(
+        appBar: AppBar(title: Text('Form Builder - ${_currentForm.formName}')),
+        body: const Center(
+          child: Column(children: [
+            Row(children: [
+              SizedBox(height: 20, child: Text("Test content!"))      
+            ])
+          ],) 
+        )
+      ),
+    );
   }
 
-  void _openFormatModal() {
-    // TODO: make this later, see the figma for the prototype
-    return;
+  
+  /// Builds the scaffold for displaying a StaffForm's [questions] .
+  ///
+  /// Takes a [StaffForm] object and returns a [Scaffold]  widget that
+  /// displays the questions in a [ListView.builder].
+  ///
+  /// Each question is displayed as a [ListTile] with the question
+  /// header as the title and the question content as the subtitle.
+  Widget buildQuestions(StaffForm form) {
+    return ListView.builder(
+      itemCount: form.questions.length,
+      itemBuilder: (context, index) {
+      Question question = form.questions[index];
+
+      /// this is where we unpack the contents of the question
+      return Card(
+      child: ListTile(
+        title: Text(question.header),
+        subtitle: Text(question.content),
+      ),
+    );
+   });
   }
 }
