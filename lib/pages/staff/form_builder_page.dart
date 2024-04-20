d8bad3793c3e7a85034e50388ff92ef1205a421c
QuantumPickleJar <vince.codefactory@outlook.com>
2024-04-20 15:18:49 -0500

    refactor: FormBuilderPage widget now uses Provider for form service and display questions in a ListView

--- lib/pages/staff/form_builder_page.dart
+++ lib/pages/staff/form_builder_page.dart
@@ -1,5 +1,9 @@
+import 'package:athlete_surveyor/models/forms/staff_form.dart';
+import 'package:athlete_surveyor/models/question.dart';
+import 'package:athlete_surveyor/services/forms/form_service.dart';
 import 'package:athlete_surveyor/widgets/ExpandedTextField.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class FormBuilderPage extends StatefulWidget {
   const FormBuilderPage({Key? key}) : super(key: key);
@@ -12,10 +16,19 @@ class FormBuilderPage extends StatefulWidget {
 class _FormBuilderPageState extends State<FormBuilderPage> {
 
   late double _sliderValue;
+  late final _formService;
+  late StaffForm _currentForm;    /// the form currently being modified
 
   /// Temporary placeholder to allow the slider to be enabled
   void _onSliderTap() {
     return;
+  }
+
+  @override 
+  void initState() {
+    super.initState();
+    _formService = Provider.of<FormService>(context, listen: true);
+
   }
 
   @override
@@ -23,100 +36,26 @@ class _FormBuilderPageState extends State<FormBuilderPage> {
     /// give [_sliderValue] a valid number
     _sliderValue = 0.5;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Form Builder'),
-      ),
-      body:  Column(
-        children: [
-          const ExpandedTextField(labelText: "Title"),
-          const Divider(),
-          const Card(
-            /// Collapsed question 1
-            margin: EdgeInsets.all(15.0),
-            child: Column(children: [
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                Text("Question 1",
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                Text(
-                  "Expand",
-                ),
-              ]),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [Text("Open-ended")],
-              )
-            ]),
-          ),
-          const Divider(),
-          const Card(
-
-              /// Expanded visible question
-              margin: EdgeInsets.all(15.0),
-              child: Column(
-                children: [
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("Question 2",
-                            style: TextStyle(fontWeight: FontWeight.bold)),
-                      ]),
-                  ExpandedTextField(labelText: "Prompt"),
-                  Divider(),
-                  Card(
-                    /// attachments card
-                    color: Color.fromRGBO(200, 200, 200, 1.0),
-                    child: Column(children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("Attachments", style: TextStyle(fontWeight: FontWeight.bold)),
-                            InputChip(
-                              label: Text("URL"),
-                              isEnabled: true,
-                              avatar: Icon(Icons.language),
-                            ),
-                            InputChip(
-                                label: Text("IMG"),
-                                isEnabled: true,
-                                avatar: Icon(Icons.upload)),
-                          ]),
-                    Divider(color: Colors.white, indent:10, endIndent: 10),
-                      /// replace with listview to show attached files
-                      Column(children: [
-                        Text("(350 x 500) - final_submit.png",
-                        style: TextStyle(color: Colors.red)),
-                        Text("Writing Guidelines - Canvas",
-                        style: TextStyle(color: Colors.red)),
-                      ]),
-                    ]),
-                  )
-                ])
-              ),
-              const Divider(),
-              /// Answer Type Selection
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                const Text("Format response:", style: TextStyle(fontWeight: FontWeight.bold)),
-                TextButton(onPressed: _openFormatModal, child: const Text("Sliding Scale"))
-              ]),/// end header row
+    return Consumer<StaffForm>(
+      builder: (context, form, child) { 
+        return Scaffold(
+          appBar: AppBar(title: Text('Form Builder - ${form.formName}')),
+          body:  ListView.builder(
+            itemCount: form.questions.length,
+            itemBuilder: (context, index) {
+              Question question = form.questions[index];
               
-              Slider(
-                  value: _sliderValue.toDouble(),
-                  onChanged: (double newValue) {
-                    setState(() {
-                      _sliderValue = newValue;
-                    });
-                  },
-                ),
-              Center(
-                child:ElevatedButton(onPressed: _openFormatModal, child: const Text("Add Question"))
-              ),
-              InputChip(onPressed: null, label: Text("Save Changes"), avatar: Icon(Icons.check))
-        ],
-      ),
-    );
+              /// this is where we unpack the contents of the question
+              return ListTile(
+                title: Text(question.header),
+                subtitle: Text(question.content),
+              );
+            }
+          ),
+          
+          
+        );
+      }
   }
 
   void _openFormatModal() {
