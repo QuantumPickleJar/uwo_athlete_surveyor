3bf7112461feee58b461a355c68307d7bcae4a75
QuantumPickleJar <vince.codefactory@outlook.com>
2024-04-19 19:57:10 -0500

    chore: commented out StaffForm for now

--- lib/models/form_factory.dart
+++ lib/models/form_factory.dart
@@ -1,7 +1,6 @@
 
-import 'package:athlete_surveyor/models/StudentForm.dart';
-import 'package:athlete_surveyor/models/StaffForm.dart';
 import 'package:athlete_surveyor/models/interfaces/IGenericForm.dart';
+import 'student_form.dart';
 
 /// Abstracts the logic away from the client, allowing a more dynamic form 
 /// creation process at runtime.
@@ -13,7 +12,7 @@ abstract class FormFactory {
 
   /// After loading form's [content], pump into form_editor_page and do 
   /// whatever is needed for entering edit mode for the form in question.
-  IGenericForm createStaffForm();
+  // IGenericForm createStaffForm();
   
 }
 
@@ -22,14 +21,15 @@ class ConcreteFormFactory extends FormFactory {
   @override
   IGenericForm createStudentForm({required String name}) => StudentForm(
     formName: name,
-    associatedSport: "",
+    sport: "",
     /// TODO: change below line, this is dirty code
     formDateReceived: DateTime.now(), /// should be read from the spreadsheet
     formDateCompleted: null
     );
 
-  @override
-  IGenericForm createStaffForm({required String name}) => StaffForm(
-    formName: ,
-  );
+
+  // @override
+  // IGenericForm createStaffForm({required String name}) => StaffForm(
+  //   return StaffF
+  // );
 }
