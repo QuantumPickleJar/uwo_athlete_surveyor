65f89b172af1d1df66d53a1e8ce89304bc78c2c5
QuantumPickleJar <vince.codefactory@outlook.com>
2024-05-01 21:34:24 -0500

    refactor: utilize new widget

--- lib/pages/staff/admin_home_page.dart
+++ lib/pages/staff/admin_home_page.dart
@@ -4,6 +4,7 @@ import 'package:athlete_surveyor/pages/staff/form_builder_page.dart';
 import 'package:athlete_surveyor/pages/staff/students_page.dart';
 import 'package:athlete_surveyor/resources/common_functions.dart';
 import 'package:athlete_surveyor/services/forms/form_service.dart';
+import 'package:athlete_surveyor/services/forms/secure_form_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -54,7 +55,8 @@ class AdminHomePage extends StatelessWidget {
                     /// push the existing form (if, for example, a previous form's thumnbnail was tapped)
                     /// otherwise send them there with a new one to be provided an ID on dbsubmittal 
                     Navigator.of(context).push(MaterialPageRoute(builder: 
-                      (context) => FormBuilderPage(formId: newForm.formId)
+                      // (context) => FormBuilderPage(formId: newForm.formId)
+                        (context) => SecureFormProvider(formId: newForm.formId)
                       ));
                     },
                   style: ButtonStyle(
