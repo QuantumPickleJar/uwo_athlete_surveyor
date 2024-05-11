16d9f1ff031b997e15cdfbac6c00cc8146537e5a
Vincent Morrill <morriv92@uwosh.edu>
2024-05-11 15:56:37 -0500

    refactor: Update MyFormsPage to use AuthoredFormsModel for displaying user's forms

--- lib/pages/common/my_forms_page.dart
+++ lib/pages/common/my_forms_page.dart
@@ -6,55 +6,24 @@ import 'package:athlete_surveyor/services/forms/form_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class MyFormsPage extends StatefulWidget {
+class MyFormsPage extends StatelessWidget {
   final LoggedInUser _currentUser;
   const MyFormsPage({super.key, required LoggedInUser currentUser}) : _currentUser = currentUser;  /// CTOR
 
   @override
-  _MyFormsPageState createState() => _MyFormsPageState();
-}
-
-class _MyFormsPageState extends State<MyFormsPage> {
-  late List<GenericForm> _forms;
-
-  @override
-  void initState() {
-    super.initState();
-    
-    /// Dropped in favor of the App Lifecycle, included for historical + referential purpose
-    // _formService = Provider.of<FormService>(context, listen: false);  /// Deprecated! 
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      print('Loading previous forms...');
-    /// as SOON as this frame completes, a [FrameCallback] is used to specify an awaited check on 
-    /// what type of user is interacting with the form, so that the app knows how to render it 
-      Provider.of<AuthoredFormsModel>(context, listen: false)
-                .getPreviousFormsFromDatabase(userId: widget._currentUser.userId!);
-    });
-    _loadForms();
-  }
-
-  Future<void> _loadForms() async {
-    if (widget._currentUser.userId != null && widget._currentUser.userId!.isNotEmpty) {
-      // _forms = await _formService.getFormsByUserId(userId: widget._currentUser.userId!);
-      
-      _forms = Provider.of<AuthoredFormsModel>(context).formsList;
-    }
-    setState(() { });
-  }
-
-  @override
   Widget build(BuildContext context) {
-
+    var authoredFormsModel = Provider.of<AuthoredFormsModel>(context);
     return ListView.builder(
-      itemCount: _forms.length,
+      itemCount: authoredFormsModel.formsList.length,
       itemBuilder: (context, index) {
         return ListTile(
-          title: Text(_forms[index].formName),
+          title: Text(authoredFormsModel.formsList[index].formName),
           onTap: () {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => FormBuilderPage(formId: _forms[index].formId),
+                builder: (context) => FormBuilderPage(
+                  formId: authoredFormsModel.formsList[index].formId),
               ),
             );
           },
