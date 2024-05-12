8259282b7a4581bfcd63ade715a5b90ca00a5022
Vincent Morrill <morriv92@uwosh.edu>
2024-05-11 19:43:08 -0500

    refactor: Update MyFormsPage to use AuthoredFormsModel for displaying user's forms

--- lib/pages/common/my_forms_page.dart
+++ lib/pages/common/my_forms_page.dart
@@ -1,14 +1,21 @@
 import 'package:athlete_surveyor/data_objects/logged_in_user.dart';
-import 'package:athlete_surveyor/models/forms/base_form.dart';
+import 'package:athlete_surveyor/models/forms/individual_form_examination_model.dart';
 import 'package:athlete_surveyor/models/forms/previous_forms_model.dart';
+import 'package:athlete_surveyor/pages/individual_form_examination_page.dart';
 import 'package:athlete_surveyor/pages/staff/form_builder_page.dart';
-import 'package:athlete_surveyor/services/forms/form_service.dart';
+import 'package:athlete_surveyor/resources/common_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+/// Page used to display to the user any [GenericForm]-type entities that
+/// have been contextually "assigned" to the user.
+/// The user can be either a student or a staff member.
+/// It is important to distinguish between the two when displaying the forms on the screen
 class MyFormsPage extends StatelessWidget {
-  final LoggedInUser _currentUser;
-  const MyFormsPage({super.key, required LoggedInUser currentUser}) : _currentUser = currentUser;  /// CTOR
+  final LoggedInUser currentUser;
+  const MyFormsPage({super.key, required this.currentUser});
+
+  /// CTOR
 
   @override
   Widget build(BuildContext context) {
@@ -16,19 +23,28 @@ class MyFormsPage extends StatelessWidget {
     return ListView.builder(
       itemCount: authoredFormsModel.formsList.length,
       itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(authoredFormsModel.formsList[index].formName),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => FormBuilderPage(
-                  formId: authoredFormsModel.formsList[index].formId),
-              ),
-            );
-          },
-        );
+        /// TOOD: replace with the yet-to-be-implemented [FormTileWidget]
+        print('bulding form ${authoredFormsModel.formsList[index].formId}...');
+        /// Shows all GenericForm-like objcets that link to this user's id
+        return _buildFormTile(context, authoredFormsModel, index);
       },
+    );
+  }
+
+  Widget _buildFormTile(
+      BuildContext context, AuthoredFormsModel formListModel, int index) {
+    return ListTile(
+      title: Text(formListModel.formsList[index].formName),
+      /// Dyanmically set the navigational route based on [currentUser]
+      onTap: () => currentUser.hasAdminPrivileges ? 
+          navigateToPage(context, FormBuilderPage(formId: formListModel.formsList[index].formId)) :
+          navigateToPage(context, IndividualFormWidget(
+            formListModel.formsList[index].formName, 
+            formListModel.formsList[index].sport, 
+            IndividualFormExaminationModel() 
+          ))
+          // _navigateToFormPage(
+          // context, authoredFormsModel.formsList[index].formId),
     );
   }
 }
