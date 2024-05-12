dcbcc74256db4bedf7bb259ce522ab11210f4f91
Vincent Morrill <morriv92@uwosh.edu>
2024-05-12 12:45:18 -0500

    add: EditQuestionWidget

--- lib/widgets/edit_question_widget.dart
+++ lib/widgets/edit_question_widget.dart
@@ -1,9 +1,25 @@
+import 'package:athlete_surveyor/models/question.dart';
 import 'package:flutter/material.dart';
     
-class EditQuestionWidget extends StatelessWidget {
-
-  const EditQuestionWidget({ Key? key }) : super(key: key);
+/// Widget responsible for containing question-manipulating operations
+/// inside of a Dialog, to keep flow of control linear 
+class EditQuestionWidget extends StatefulWidget {
+  final Question question;
+  final Function(Question) onSave;    /// what should happen to the question when done
+  final Function onCancel;            /// what (if anything) should happen on cancellation
   
+  const EditQuestionWidget({ 
+    Key? key, 
+    required this.question,
+    required this.onSave,
+    required this.onCancel,
+     }) : super(key: key);
+
+  @override
+  State<EditQuestionWidget> createState() => _EditQuestionWidgetState();
+}
+
+class _EditQuestionWidgetState extends State<EditQuestionWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
