3e655cae87ec05a069bb79c9e6edf38471fc59cf
Vincent Morrill <morriv92@uwosh.edu>
2024-05-12 12:34:11 -0500

    refactor: Update MyFormsPage to use AuthoredFormsModel for displaying user's forms

--- lib/pages/common/my_forms_page.dart
+++ lib/pages/common/my_forms_page.dart
@@ -4,6 +4,7 @@ import 'package:athlete_surveyor/models/forms/previous_forms_model.dart';
 import 'package:athlete_surveyor/pages/individual_form_examination_page.dart';
 import 'package:athlete_surveyor/pages/staff/form_builder_page.dart';
 import 'package:athlete_surveyor/resources/common_functions.dart';
+import 'package:athlete_surveyor/resources/common_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -20,14 +21,17 @@ class MyFormsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var authoredFormsModel = Provider.of<AuthoredFormsModel>(context);
-    return ListView.builder(
-      itemCount: authoredFormsModel.formsList.length,
-      itemBuilder: (context, index) {
-        /// TOOD: replace with the yet-to-be-implemented [FormTileWidget]
-        print('bulding form ${authoredFormsModel.formsList[index].formId}...');
-        /// Shows all GenericForm-like objcets that link to this user's id
-        return _buildFormTile(context, authoredFormsModel, index);
-      },
+    return Scaffold(
+      appBar: defaultAppBar(buildContext: context, hasBackButton: true, title: 'My Forms (${authoredFormsModel.formsList.length})'),
+      body: ListView.builder(
+        itemCount: authoredFormsModel.formsList.length,
+        itemBuilder: (context, index) {
+          /// TOOD: replace with the yet-to-be-implemented [FormTileWidget]
+          print('bulding form ${authoredFormsModel.formsList[index].formId}...');
+          /// Shows all GenericForm-like objcets that link to this user's id
+          return _buildFormTile(context, authoredFormsModel, index);
+        },
+      ),
     );
   }
 
@@ -35,6 +39,7 @@ class MyFormsPage extends StatelessWidget {
       BuildContext context, AuthoredFormsModel formListModel, int index) {
     return ListTile(
       title: Text(formListModel.formsList[index].formName),
+
       /// Dyanmically set the navigational route based on [currentUser]
       onTap: () => currentUser.hasAdminPrivileges ? 
           navigateToPage(context, FormBuilderPage(formId: formListModel.formsList[index].formId)) :
