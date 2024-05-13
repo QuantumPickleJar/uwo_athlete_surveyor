299c4dedf1a3d0b6729426408cb66f0a74eb0b46
Vincent Morrill <morriv92@uwosh.edu>
2024-05-13 18:17:13 -0500

    refactor: pulled in DB changes

--- lib/data_objects/logged_in_user.dart
+++ lib/data_objects/logged_in_user.dart
@@ -4,9 +4,6 @@ class LoggedInUser
   final bool _isAdmin;
   final bool _isTempPassword;
   final String _username;
-
-  /// Nullable so that we can treat the auto-generated emails as unique identifiers
-  final String? _userId;
   final String _firstName;
   final String _lastName;
 
@@ -15,24 +12,6 @@ class LoggedInUser
   String get firstName => _firstName;
   String get lastName => _lastName;
   String get email => _username;
-  String? get userId => _userId;
 
-  LoggedInUser(
-    this._userId,
-    this._isAdmin, 
-    this._isTempPassword, 
-    this._username, this._firstName, this._lastName, 
-    );
+  LoggedInUser(this._isAdmin, this._isTempPassword, this._username, this._firstName, this._lastName);
 }
-
-// class Staff extends LoggedInUser {
-//   Staff({required super.userId, required super.username, required super.firstName, required super.lastName});
-  
-//   // Additional staff-specific methods here
-// }
-
-// class Student extends LoggedInUser {
-//   Student({required super.userId, required super.username, required super.firstName, required super.lastName});
-  
-//   // Additional staff-specific methods here
-// }
--- lib/database.dart
+++ lib/database.dart
@@ -1,23 +1,23 @@
 // ignore_for_file: avoid_print
+
 import 'package:postgres/postgres.dart';
 
 /// Static database class to handle all SQL transactions.
+/// TODO: consolidate common SQL query methods
 class Database 
 {
-  // Database connection credentials.
   static const String _dbHost = "cs361-lab3-13043.5xj.gcp-us-central1.cockroachlabs.cloud";
   static const int _dbPort = 26257;
   static const String _dbName = "uwo_forms_docs_test";
   static const String _dbUser = "joshhill";
   static const String _dbPass = "0LMiuWwPzCfrlub7YlKxpw";
-  // SQL fetch query strings.
+
   static const String _getEmailsQuery = "SELECT date_received, address_from, subject_line, body FROM tbl_inbox;";
   static const String _getStudentList = "SELECT student_name, grade, sport FROM tbl_studentList"; 
   static const String _getPreviousFormsQuery = "SELECT form_name, associated_sport, date_received, date_completed FROM tbl_previous_forms_temp;";
-  static const String _getUserPassword = "SELECT * FROM tbl_users WHERE username = @username";
-  // SQL insert query strings. ***Note on inserts; add "RETURNING <column_name>" to end of insert queries to get a Result from them, with <column name> usually being the ID that gets assigned to it.
   static const String _insertAthlete = "INSERT INTO tbl_studentlist (student_name, grade, sport) VALUES (@studentName, @grade, @sport)";
-  static const String _insertNewUser = "INSERT INTO tbl_users (username,password,is_admin) VALUES (@username,@password,@is_admin) RETURNING uuid_user";
+  static const String _getUserPassword = "SELECT * FROM tbl_users WHERE username = @username";
+  static const String _insertNewUser = "INSERT INTO tbl_users (username,password,is_admin) VALUES (@username,@password,@is_admin)";
 
   /// Open connection to the database.
   static Future<Connection> getOpenConnection() async 
@@ -38,24 +38,19 @@ class Database
     ); 
   }
 
-  /// Generic SQL execution method; just need to supply query and any parameters which should exist as constants in this file.
-  static Future<Result> executeSQLCommand(String query, Object? optionalParameters) async
-  {
-    Connection? conn;
+  /// Get all Emails from the database.
+  static Future<Result> fetchEmails() async 
+  { 
+    Connection? conn; 
     try 
     { 
       conn = await getOpenConnection(); 
-      final Result result = await conn.execute
-      (
-        Sql.named(query),
-        parameters: optionalParameters
-      );
-
+      final Result result = await conn.execute(_getEmailsQuery);
       return result;
     } 
     catch (e) 
     { 
-      print('Error executing SQL command: $e'); 
+      print('Error fetching data: $e'); 
       rethrow;
     } 
     finally 
@@ -65,32 +60,119 @@ class Database
   }
 
   /// Get all Emails from the database.
-  static Future<Result> fetchEmails() async { return executeSQLCommand(_getEmailsQuery,null); }
-
-  /// Get all previously completed forms from the database.
-  static Future<Result> fetchPreviousForms() async { return executeSQLCommand(_getPreviousFormsQuery,null); }
-
-  /// Fetching the student list
-  static Future<Result> fetchStudents() async { return executeSQLCommand(_getStudentList,null); }
-
-  /// Get a user's password based on their provided username (usually to check against the password they provided for login).
-  static Future<Result> fetchUserPassword(String username) async
-  {
-    return executeSQLCommand(_getUserPassword, 
-                            {'username':username});
+  static Future<Result> fetchPreviousForms() async 
+  { 
+    Connection? conn; 
+    try 
+    { 
+      conn = await getOpenConnection(); 
+      final Result result = await conn.execute(_getPreviousFormsQuery);
+      return result;
+    } 
+    catch (e) 
+    { 
+      print('Error fetching data: $e'); 
+      rethrow;
+    } 
+    finally 
+    { 
+      conn?.close();
+    } 
   }
 
-  /// Connects the app to the data base and gets the athletes put in on the app into the database
-  static Future<Result> insertAthlete(String studentName, String grade, String sport) async 
+  //fetching the student list
+  static Future<Result> fetchStudents() async 
+  { 
+    Connection? conn; 
+    try 
+    { 
+      conn = await getOpenConnection(); 
+      final Result result = await conn.execute(_getStudentList);
+      return result;
+    } 
+    catch (e) 
+    { 
+      print('Error fetching data: $e'); 
+      rethrow;
+    } 
+    finally 
+    { 
+      conn?.close();
+    } 
+  }
+
+  //connects the app to the data base and gets the athletes put in on the app into the database
+  static Future<void> insertAthlete(String studentName, String grade, String sport) async 
   {
-    return executeSQLCommand(_insertAthlete, 
-                            {'student_name':studentName, 'grade':grade, 'sport':sport});
+    Connection? conn;
+    try
+    {
+      conn = await getOpenConnection(); 
+      await conn.execute
+      (
+        Sql.named(_insertAthlete),
+        parameters: {'student_name': studentName, 'grade': grade, 'sport': sport}
+      );
+        print('Connected successfully.');
+    }
+    catch (e)
+    {
+      print('Error inserting data: $e');
+      rethrow;
+    }
+    finally
+    {
+      conn?.close();
+    } 
+  }
+
+  /// Get a user's password based on their provided username (usually to check against the password they provided for login).
+  static Future<Result> fetchUserPassword(String userName) async
+  {
+    Connection? conn;
+    try
+    {
+      conn = await getOpenConnection(); 
+      return await conn.execute
+      (
+        Sql.named(_getUserPassword),
+        parameters: {'username': userName}
+      );
+    }
+    catch (e)
+    {
+      print('Error fetching data: $e');
+      rethrow;
+    }
+    finally
+    {
+      conn?.close();
+    } 
   }
 
   /// Insert a new user to the DB; bool response shows whether or not it was successful.
-  static Future<Result> insertNewUser(String username, String password, bool isAdmin) async
+  static Future<bool> insertNewUser(String username, String password, bool isAdmin) async
   {
-    return executeSQLCommand(_insertNewUser, 
-                            {'username': username, 'password': password, 'is_admin': isAdmin});
+    Connection? conn;
+    try
+    {
+      conn = await getOpenConnection(); 
+      await conn.execute
+      (
+        Sql.named(_insertNewUser),
+        parameters: {'username': username, 'password': password, 'is_admin': isAdmin}
+      );
+    }
+    catch (e)
+    {
+      print('Error inserting data: $e');
+      return false; // insertion not successful
+    }
+    finally
+    {
+      conn?.close();
+    } 
+
+    return true;
   }
 }
