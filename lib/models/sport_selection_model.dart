1037c876a298cf2341188496054da557ac73b623
Vincent Morrill <morriv92@uwosh.edu>
2024-05-14 22:12:31 -0500

    feat: Update SportSelectionModel to load sports from SportsRepository

--- lib/models/sport_selection_model.dart
+++ lib/models/sport_selection_model.dart
@@ -2,14 +2,26 @@ import 'package:athlete_surveyor/models/sport.dart';
 import 'package:athlete_surveyor/services/sports/sports_repository.dart';
 import 'package:flutter/material.dart';
 /// model that sticks to the [SportPickerCard]. 
-/// Tracks possible choices from the database, and (when 
-/// in the context of building a form) stores current choice
+/// 
+/// Tracks possible choices from the database, and (when  in the context
+/// of building a form) stores current choice. 
+/// 
+/// Loosely follows the Facade pattern in that we're abstracting the DB
+/// interaction to the [SportSelectionModel]
 class SportSelectionModel extends ChangeNotifier {
-  late List<Sport> _sports;
   
+  final SportsRepository _sportsRepository;
+  List<Sport>? sports;
   Sport? _selectedSport; 
-  List<Sport> get sports => _sports;
   Sport? get selectedSport => _selectedSport;
+
+    
+  SportSelectionModel(this._sportsRepository);
+
+  Future<void> loadSports() async {
+    sports = await _sportsRepository.getAllSports();
+    notifyListeners();
+  }
 
   void selectSport(Sport sport) {
     _selectedSport = sport;
