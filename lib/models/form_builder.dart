681788da593b4c1b23eb98795d3fdf7737ba0c9a
Vincent Morrill <morriv92@uwosh.edu>
2024-05-14 19:38:19 -0500

    refactor: Add SportsRepository for interacting with stored sports data

--- lib/models/form_builder.dart
+++
@@ -1,6 +0,0 @@
-/**
- * builds Forms from their contents assembled from its stored representation
- * in google Sheets.
- * 
- * Ideally we can leverage the factory pattern to 
- */
---
+++ lib/services/sports/sports_repository.dart
@@ -0,0 +1,54 @@
+import 'package:athlete_surveyor/models/sport.dart';
+import 'package:postgres/postgres.dart';
+import 'package:athlete_surveyor/services/db.dart';
+
+/// Simple repository for interacting with stored values for the available sports to pick from
+class SportRepository {
+  Future<Connection> get _connection async => await PostgresDB.getConnection();
+  /// We could put a list here since sports probably wont get added too often after initial setup
+  
+  SportRepository();
+
+  Future<List<Sport>> getAllSports() async {
+    var db = await _connection;
+    try {
+      String sqlStatement = "SELECT sport_id, sport_name FROM public.tbl_sports;";
+      var result = await db.execute(Sql.named(sqlStatement));
+      return result.map((row) => Sport(
+        sportId: row.toColumnMap()['sport_id'],
+        activity: row.toColumnMap()['sport_name'],
+      )).toList();
+    } finally {
+      PostgresDB.closeConnection();
+    }
+  }
+  /// Fetches a single sport by ID
+  Future<String?> getSportById(int sportId) async {
+    var db = await _connection;
+    try {
+      String sql = 'SELECT sport_name FROM tbl_sports WHERE sport_id = @sportId;';
+      var results = await db.execute(sql, parameters: {'sportId': sportId});
+      if (results.isNotEmpty) {
+        return results.first[0] as String;
+      }
+      return null;
+    } finally {
+      db.close();
+    }
+  }
+
+  /// Fetches a single sport by name
+  Future<int?> getSportIdByName(String sportName) async {
+    var db = await _connection;
+    try {
+      String sql = 'SELECT sport_id FROM tbl_sports WHERE sport_name = @sportName;';
+      var results = await db.execute(sql, parameters: {'sportName': sportName});
+      if (results.isNotEmpty) {
+        return results.first[0] as int;
+      }
+      return null;
+    } finally {
+      db.close();
+    }
+  }
+}
