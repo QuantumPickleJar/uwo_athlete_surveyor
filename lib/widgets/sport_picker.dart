64b93de06af18e50afa92e3c8d0329796bfcb1fc
Vincent Morrill <morriv92@uwosh.edu>
2024-05-15 12:57:01 -0500

    refactor: Update SportPickerCard to use Provider for accessing SportsRepository, now Stateless

--- lib/widgets/sport_picker.dart
+++ lib/widgets/sport_picker.dart
@@ -1,61 +1,52 @@
 import 'package:athlete_surveyor/models/sport.dart';
+import 'package:athlete_surveyor/models/sport_selection_model.dart';
 import 'package:athlete_surveyor/services/sports/sports_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class SportPickerCard extends StatefulWidget {
-  final void Function(Sport?) onSportChanged;
+class SportPickerCard extends StatelessWidget {
+  // final void Function(Sport?) onSportChanged;
 
-  const SportPickerCard({
-    Key? key,
-    required this.onSportChanged,
-  }) : super(key: key);
+  SportPickerCard({super.key});
+  // Sport? _selectedSport; /// held by the [SportSelectionModel]
 
-  @override
-  _SportPickerCardState createState() => _SportPickerCardState();
-}
-
-class _SportPickerCardState extends State<SportPickerCard> {
-  late SportsRepository sportsRepository; 
-  Sport? _selectedSport;
-
-  @override initState() {
-    sportsRepository = Provider.of<SportsRepository>(context, listen: false);
-  }
 
   @override
-  Widget build(BuildContext context) async {
-    List<Sport> sports = await sportsRepository.getAllSports();
+  Widget build(BuildContext context) {
+    // SportSelectionModel model = Provider.of<SportSelectionModel>(context);
+    
+    return Consumer<SportSelectionModel>(
+      builder: (context, sportSelectionModel, child) {
 
-    return Card(
-      margin: const EdgeInsets.all(16.0),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("Select a sport"),
-            const SizedBox(height: 16.0),
-            DropdownButton<Sport>(
-              value: _selectedSport,
-              hint: const Text("Choose a sport"),
-              onChanged: (Sport? newValue) {
-                setState(() {
-                  _selectedSport = newValue;
-                  widget.onSportChanged(newValue);
-                });
-              },
-              items: sports.map((Sport sport) {
-                return DropdownMenuItem<Sport>(
-                  value: sport,
-                  child: Text(sport.name),
-                );
-              }).toList(),
-            ),
-          ],
+      return Card(
+        margin: const EdgeInsets.all(16.0),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Select a sport"),
+              const SizedBox(height: 16.0),
+              DropdownButton<Sport>(
+                value: sportSelectionModel.selectedSport,
+                hint: const Text("Choose a sport"),
+                items: sportSelectionModel.sports.map((Sport sport) {
+                  return DropdownMenuItem<Sport>(
+                    value: sport,
+                    child: Text(sport.activity),
+                  );
+                }).toList(),
+                onChanged: (Sport? newSport) {
+                  if (newSport != null) {
+                    sportSelectionModel.selectSport(newSport);
+                  }
+                }
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
