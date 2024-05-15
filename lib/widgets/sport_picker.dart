857e456cd4eae1ae634c47dc9f30531f1d30ad27
Vincent Morrill <morriv92@uwosh.edu>
2024-05-14 21:48:40 -0500

    feat: Add SportPickerCard widget for selecting a sport

---
+++ lib/widgets/sport_picker.dart
@@ -0,0 +1,56 @@
+import 'package:athlete_surveyor/models/sport.dart';
+import 'package:athlete_surveyor/services/sports/sports_repository.dart';
+import 'package:flutter/material.dart';
+
+class SportPickerCard extends StatefulWidget {
+  final void Function(Sport?) onSportChanged;
+
+  const SportPickerCard({
+    Key? key,
+    required this.onSportChanged,
+  }) : super(key: key);
+
+  @override
+  _SportPickerCardState createState() => _SportPickerCardState();
+}
+
+class _SportPickerCardState extends State<SportPickerCard> {
+  final SportRepository sportsRepository = SportRepository();
+  Sport? _selectedSport;
+
+  @override
+  Widget build(BuildContext context) async {
+    List<Sport> sports = await sportsRepository.getAllSports();
+
+    return Card(
+      margin: const EdgeInsets.all(16.0),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Select a sport"),
+            const SizedBox(height: 16.0),
+            DropdownButton<Sport>(
+              value: _selectedSport,
+              hint: const Text("Choose a sport"),
+              onChanged: (Sport? newValue) {
+                setState(() {
+                  _selectedSport = newValue;
+                  widget.onSportChanged(newValue);
+                });
+              },
+              items: sports.map((Sport sport) {
+                return DropdownMenuItem<Sport>(
+                  value: sport,
+                  child: Text(sport.name),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
