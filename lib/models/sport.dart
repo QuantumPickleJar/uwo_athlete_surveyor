a4ab51cfecf3c24ba7904a33cdb3175bb7188a0b
Vincent Morrill <morriv92@uwosh.edu>
2024-05-16 18:26:27 -0500

    refactor: Update Sport model with JSON parsing functions

--- lib/models/sport.dart
+++ lib/models/sport.dart
@@ -2,5 +2,22 @@ class Sport {
   final int sportId;
   final String activity;
 
+  /// basic class, with a slightly-less-than basic pair of json functions
+  /// to assist us in manuevering around a blocking postgres bug.
   Sport({required this.sportId, required this.activity});
+
+  /// Static parsing function, used to read sport objects from assets
+
+ /// Factory constructor to create a Sport object from JSON
+  factory Sport.fromJson(Map<String, dynamic> json) {
+    return Sport(
+      sportId: int.parse(json['sport_id']),
+      activity: json['activity'],
+    );
+  }
+
+  /// Static method to parse a list of sports from JSON
+  static List<Sport> fromJsonList(List<dynamic> jsonList) {
+    return jsonList.map((item) => Sport.fromJson(item)).toList();
+  }
 }
