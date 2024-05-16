e9dc25b5da8d384c7d2b2702384755b8e8bea9b7
Vincent Morrill <morriv92@uwosh.edu>
2024-05-16 18:21:14 -0500

    refactor: Update SportsRepository to use 'activity' instead of 'sport_name' in SQL queries

--- lib/services/sports/sports_repository.dart
+++ lib/services/sports/sports_repository.dart
@@ -15,27 +15,11 @@ import 'package:provider/provider.dart';
 class SportsRepository {
   Future<Connection> get _connection async => await PostgresDB.getConnection();
   /// We could put a list here since sports probably wont get added too often after initial setup
-  final SportSelectionModel sportsModel;
-  
+ 
 
   /// workaround constructor to bypass the need for a transaction query
   /// (allows us to shift the second)
-  SportsRepository({required this.sportsModel}) {
-    /// upon construction, check if [SportSelectionModel] is in the widget tree
-    /// sportsModel = Provider.of<SportSelectionModel>(context, listen: false);
-      _initialize();
-    }
-
-  /// Sets up the databases' sports so that the forms can be set up-front with them
-  Future<void> _initialize() async {
-    try {
-      var sports = await getAllSports();
-      debugPrint('[SportsRespository] Fetched ${sports.length} sports.');
-      sportsModel.sports = sports;
-    } catch (e) {
-      debugPrint('Error initializing SportsRepository: $e');
-    }
-  }
+  SportsRepository();
 
   /// Fetches all Sports from the database, returning them in a Future as a List<Sport>
   Future<List<Sport>> getAllSports() async {
@@ -48,13 +32,13 @@ class SportsRepository {
         activity: row.toColumnMap()['activity'],
       )).toList();
     } finally {
-       await db.close();
+       await PostgresDB.closeConnection();
 
     }
   }
   /// Fetches a single sport by ID
   Future<String?> getSportById(String sportId) async {
-    var db = await _connection;
+    var db = await PostgresDB.getConnection();
 
     try {
       String sql = 'SELECT activity FROM tbl_sports WHERE sport_id = @sportId;';
@@ -64,13 +48,13 @@ class SportsRepository {
       }
       return null;
     } finally {
-      await db.close();
+      await PostgresDB.closeConnection();
     }
   }
 
 
   Future<String?> getSportIdByName(String sportName) async {
-    var db = await _connection;
+    var db = await PostgresDB.getConnection();
     try {
       debugPrint('Connecting to DB for sportName: $sportName');
       String sql = 'SELECT sport_id FROM tbl_sports WHERE activity = @sportName;';
@@ -88,7 +72,7 @@ class SportsRepository {
       rethrow;
     } finally {
       try {
-        await db.close();
+        await PostgresDB.closeConnection();
       } catch (closeError) {
         debugPrint('Error closing the database connection: $closeError');
       }
