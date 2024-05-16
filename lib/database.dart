e22beb5cd0581dcba872924be5f99e4d869a5abe
joshua-t-hill <hill.joshuataylor@gmail.com>
2024-05-16 18:47:39 -0500

    Moving changes between devices without committing them to dev

--- lib/database.dart
+++ lib/database.dart
@@ -6,27 +6,38 @@
 /// Bugs: n/a
 /// Reflection: left the const Strings here instead of moving to constants file as it didn't feel appropriate.
 
+import 'package:athlete_surveyor/models/response_type.dart';
 import 'package:postgres/postgres.dart';
 
 /// Static database class to handle all SQL transactions.
 class Database 
 {
-  // Database connection credentials.
+  /// Database connection credentials.
   static const String _dbHost = "cs361-lab3-13043.5xj.gcp-us-central1.cockroachlabs.cloud";
   static const int _dbPort = 26257;
   static const String _dbName = "uwo_forms_docs_test";
   static const String _dbUser = "joshhill";
   static const String _dbPass = "0LMiuWwPzCfrlub7YlKxpw";
-  // SQL fetch query strings.
-  static const String _getEmailsQuery = "SELECT date_received, from_uuid, subject_line, body, first_name, last_name FROM tbl_inbox ti LEFT JOIN tbl_users tu ON ti.from_uuid = tu.uuid_user;";
+  /// SQL fetch query strings.
+  static const String _getEmails = "SELECT date_received, from_uuid, subject_line, body, first_name, last_name FROM tbl_inbox ti LEFT JOIN tbl_users tu ON ti.from_uuid = tu.uuid_user;";
   static const String _getStudentList = "SELECT student_name, grade, sport, student_id FROM tbl_studentList"; 
-  static const String _getPreviousFormsQuery = "SELECT form_name, associated_sport, date_received, date_completed FROM tbl_previous_forms_temp;";
+  static const String _getPreviousForms = "SELECT form_name, associated_sport, date_received, date_completed FROM tbl_previous_forms_temp;";
   static const String _getSpecificUser = "SELECT * FROM tbl_users WHERE username = @username";
-  // SQL insert query strings. ***Note on inserts; add "RETURNING <column_name>" to end of insert queries to get a Result from them, with <column name> usually being the ID that gets assigned to it.
+  static const String _getQuestionByQuestionId = """SELECT content, order_in_form, form_id, question_id, response_enum FROM public.tbl_questions WHERE question_id = @questionId;""";
+  static const String _getAllQuestionsQuery = """SELECT * FROM tbl_questions""";
+  static const String _getAllQuestionsByFormId = """SELECT * from tbl_questions WHERE form_id = @formId""";
+  /// SQL insert query strings. ***Note on inserts; add "RETURNING <column_name>" to end of insert queries to get a Result from them, with <column name> usually being the ID that gets assigned to it.
   static const String _insertAthlete = "INSERT INTO tbl_studentlist (student_name, grade, sport, student_id) VALUES (@studentName, @grade, @sport, @id)";
   static const String _insertNewUser = "INSERT INTO tbl_users (username,password,first_name,last_name,is_admin) VALUES (@username,@password,@first_name,@last_name,@is_admin) RETURNING uuid_user";
+  static const String _insertNewQuestionByFormId = """INSERT INTO public.tbl_questions (content, order_in_form, form_id, response_enum VALUES (@content, @order, @formId, @responseEnum) RETURNING *;""";
+  static const String _insertNewQuestions = "INSERT INTO public.tbl_questions (content, order_in_form, form_id, response_enum) VALUES @values";
+  /// SQL delete query strings.
   //static const String _deleteAthlete = " DElETE FROM tbl_studentlist WHERE student_name =  @name AND grade = @grade AND sport = @sport";
-  
+  static const String _deleteQuestion = """DELETE * FROM public.tbl_questions WHERE question_id = @questionId;""";
+  static const String _deleteForm = "DELETE FROM public.tbl_questions WHERE form_id = @formId";
+  /// SQL update query strings.
+  static const String _updateQuestion = """UPDATE public.tbl_questions SET content = @content, order_in_form = @order, form_id = @formId, response_enum = @responseEnum WHERE question_id = @questionId;""";
+
   /// Open connection to the database.
   static Future<Connection> getOpenConnection() async 
   { 
@@ -47,7 +58,7 @@ class Database
   }
 
   /// Generic SQL execution method; just need to supply query and any parameters which should exist as constants in this file.
-  static Future<Result> executeSQLCommand(String query, Object? optionalParameters) async
+  static Future<Result> _executeSQLCommand(String query, Object? optionalParameters) async
   {
     Connection? conn;
     try 
@@ -73,32 +84,84 @@ class Database
   }
 
   /// Get all Emails from the database.
-  static Future<Result> fetchEmails() async { return executeSQLCommand(_getEmailsQuery,null); }
+  static Future<Result> fetchEmails() async { return _executeSQLCommand(_getEmails,null); }
 
   /// Get all previously completed forms from the database.
-  static Future<Result> fetchPreviousForms() async { return executeSQLCommand(_getPreviousFormsQuery,null); }
+  static Future<Result> fetchPreviousForms() async { return _executeSQLCommand(_getPreviousForms,null); }
 
   /// Fetching the student list
-  static Future<Result> fetchStudents() async { return executeSQLCommand(_getStudentList,null); }
+  static Future<Result> fetchStudents() async { return _executeSQLCommand(_getStudentList,null); }
+
+  /// 
+  static Future<Result> fetchAllQuestions() async { return _executeSQLCommand(_getAllQuestionsQuery,null); }
 
   /// Get a user's profile based on their provided username.
   static Future<Result> fetchUser(String username) async
   {
-    return executeSQLCommand(_getSpecificUser, 
-                            {'username':username});
+    return _executeSQLCommand(_getSpecificUser, 
+                             {'username':username});
+  }
+
+  /// Get question with matching Id.
+  static Future<Result> fetchQuestionByQuestionId(String questionId) async
+  {
+    return _executeSQLCommand(_getQuestionByQuestionId, 
+                             {'questionId':questionId});
+  }
+
+  ///
+  static Future<Result> fetchQuestionsByFormId(String formId) async
+  {
+    return _executeSQLCommand(_getAllQuestionsByFormId, 
+                             {'formId':formId });
   }
 
   /// Connects the app to the data base and gets the athletes put in on the app into the database
   static Future<Result> insertAthlete(String studentName, String grade, String sport, String id) async 
   {
-    return executeSQLCommand(_insertAthlete, 
-                            {'student_name':studentName, 'grade':grade, 'sport':sport, 'student_id': id});
+    return _executeSQLCommand(_insertAthlete, 
+                             {'student_name':studentName, 'grade':grade, 'sport':sport, 'student_id': id});
   }
 
   /// Insert a new user to the DB; returns the new user's DB UUID for reference.
   static Future<Result> insertNewUser(String username, String password, String firstName, String lastName, bool isAdmin) async
   {
-    return executeSQLCommand(_insertNewUser, 
-                            {'username':username, 'password':password, 'first_name':firstName, 'last_name':lastName, 'is_admin':isAdmin});
+    return _executeSQLCommand(_insertNewUser, 
+                             {'username':username, 'password':password, 'first_name':firstName, 'last_name':lastName, 'is_admin':isAdmin});
+  }
+
+  /// Insert a new question into the DB associated with a form that has an Id of formId.
+  static Future<Result> insertNewQuestionByFormId(String questionContent, int? questionOrdinal, String formId, ResponseWidgetType questionResFormat) async
+  {
+    return _executeSQLCommand(_insertNewQuestionByFormId, 
+                             {'content':questionContent, 'order':questionOrdinal, 'formId':formId, 'responseEnum':questionResFormat});
+  }
+
+  /// 
+  static Future<Result> insertNewQuestions(String concatenatedValues) async
+  {
+    return _executeSQLCommand(_insertNewQuestions, 
+                             {'values':concatenatedValues});   
+  }
+
+  /// Removes a question from the database by its id. 
+  static Future<Result> deleteQuestion(String questionId) async
+  {
+    return _executeSQLCommand(_deleteQuestion,
+                             {'questionId':questionId});
+  }
+
+  /// 
+  static Future<Result> deleteFormById(String formId) async
+  {
+    return _executeSQLCommand(_deleteForm,
+                             {'formId':formId});
+  }
+
+  /// 
+  static Future<Result> updateQuestion(String questionContent, int? questionOrdinal, String questionFormId, ResponseWidgetType questionResFormat, String questionId) async
+  {
+    return _executeSQLCommand(_updateQuestion, 
+                             {'content':questionContent, 'order':questionOrdinal, 'formId':questionFormId, 'responseEnum':questionResFormat, 'questionId':questionId});
   }
 }
--- lib/services/questions/question_repository.dart
+++ lib/services/questions/question_repository.dart
@@ -9,157 +9,103 @@
 import 'package:athlete_surveyor/models/interfaces/i_question_respository.dart';
 import 'package:athlete_surveyor/models/question.dart';
 import 'package:athlete_surveyor/models/response_type.dart';
-import 'package:athlete_surveyor/services/db.dart';
-import 'package:postgres/postgres.dart';
+import 'package:athlete_surveyor/database.dart';
 
-class QuestionRepository implements IQuestionRepository{
-  Future<Connection> get _connection async => await PostgresDB.getConnection();
-  // final Connection _connection;
-  // static QuestionRepository? _db_instance;
-
+class QuestionRepository implements IQuestionRepository
+{
   // Private constructor, facilitates safeguarded connection 
   QuestionRepository();
 
+  /// 
   @override
-  Future<Question> createQuestion(Question question, String formId) async {
-    String sql = """INSERT INTO public.tbl_questions (content, order_in_form, form_id, response_enum
-                        VALUES (@content, @order, @formId, @responseEnum) 
-                        RETURNING *;""";
-    var db = await _connection; /// get the static connection
-    var result = await db.execute(
-      Sql.named(sql), parameters: {
-      'content': question.content,
-      'order': question.ordinal,
-      'formId': formId,
-      'responseEnum': question.resFormat.widgetType // Assuming you have an enum mapped to int
-    });
+  Future<Question> createQuestion(Question question, String formId) async 
+  {
+    var result = await Database.insertNewQuestionByFormId(question.content,
+                                                  question.ordinal,
+                                                  formId,
+                                                  question.resFormat.widgetType); // Assuming you have an enum mapped to int
 
-    if (result.isEmpty) {
-      throw Exception('Failed to create question.');
-    }
+    if (result.isEmpty) { throw Exception('Failed to create question.'); }
+
     return _mapRowToQuestion(result.first.toColumnMap());
   }
 
   /// Removes a question from the database by its id. 
   @override
-  Future<bool> deleteQuestion(String questionId) async {
-    try {
-      // TODO: implement deleteQuestion
-      String sqlStatement = """DELETE * FROM public.tbl_questions WHERE question_id = @questionId;""";
-      var db = await _connection; /// get the static 
-      var result = await db.execute(sqlStatement, parameters: { 'questionId' : questionId});
-      if (result.isEmpty) {
-        return false;
-      } else {
-        return true;
-      }
-    } finally {
-      PostgresDB.closeConnection();
-    }
+  Future<bool> deleteQuestion(String questionId) async 
+  {
+    // TODO: implement deleteQuestion
+    var result = await Database.deleteQuestion(questionId);
+
+    return result.isEmpty;
   }
 
+  /// 
   @override
-  Future<Question?> getQuestionById(String questionId) async {
-    try {
-      // TODO: implement getQuestionById
-      String sqlStatement = """SELECT content, order_in_form, form_id, question_id, response_enum
-                              FROM public.tbl_questions WHERE question_id = @questionId;""";
-      var db = await _connection; /// get the static connection
-      var result = await db.execute(sqlStatement, parameters: { 'questionId' : questionId});
-      if (result.isEmpty) {
-        return null;
-      } else {
-        return _mapRowToQuestion(result.first.toColumnMap());
-      }
-    } finally {
-      PostgresDB.closeConnection();
-    }
+  Future<Question?> getQuestionById(String questionId) async 
+  {
+    // TODO: implement getQuestionById
+    var result = await Database.fetchQuestionByQuestionId(questionId);
+
+    return result.isEmpty ? null 
+                          : _mapRowToQuestion(result.first.toColumnMap());
   }
 
-  @override Future<List<Question>> getQuestions() async {
-    try {
-       // TODO: implement getQuestions
-      String sqlStatement = """SELECT * FROM tbl_questions""";
-      var db = await _connection; /// get the static connection
-      var result = await db.execute(sqlStatement);
-        
-      /// unpack the result by leveraging the row mapping function (since we have a List of Questions)
-      return result.map((row) => _mapRowToQuestion(row.toColumnMap())).toList();
-    } finally {
-      PostgresDB.closeConnection();
-    }
+  /// 
+  @override
+  Future<List<Question>> getQuestions() async 
+  {
+    // TODO: implement getQuestions
+    var result = await Database.fetchAllQuestions();
+
+    // Unpack the result by leveraging the row mapping function (since we have a List of Questions)
+    return result.map((row) => _mapRowToQuestion(row.toColumnMap())).toList();
   }
 
   /// Fetches all of the questions that belong to a form, by their [formId].  
   /// TODO: migrate the `form_id` col from `tbl_questions` to a junc table `tbl_form_question`
-  @override Future<List<Question>> resolveQuestionsByFormId({required String formId}) async {
-    try {
-      String sqlStatement = """SELECT * from tbl_questions WHERE form_id = @formId""";
-      //   String sqlStatement = """SELECT content, order_in_form, form_id, question_id, response_enum
-      //                        FROM public.tbl_questions WHERE question_id = @questionId;""";
-      var db = await _connection; /// get the static connection
-      var result = await db.execute(Sql.named(sqlStatement), parameters: { 'formId': formId });
-        
-      /// unpack the result by leveraging the row mapping function (since we have a List of Questions)
-      return result.map((row) => _mapRowToQuestion(row.toColumnMap())).toList();
-    } finally {
-      PostgresDB.closeConnection();
-    }
+  Future<List<Question>> resolveQuestionsByFormId({required String formId}) async 
+  {
+    var result = await Database.fetchQuestionsByFormId(formId);
+
+    // Unpack the result by leveraging the row mapping function (since we have a List of Questions)
+    return result.map((row) => _mapRowToQuestion(row.toColumnMap())).toList();
   }
 
-
+  /// 
   @override
-  Future<void> updateQuestion(Question question) async {
-    try {
-      // TODO: implement updateQuestion
-      String sqlStatement = """UPDATE public.tbl_questions
-                              SET content = @content, order_in_form = @order,
-                                  form_id = @formId, response_enum = @responseEnum
-                              WHERE question_id = @questionId;""";
-  var db = await _connection; /// get the static connection
-      var result = await db.execute(      
-        Sql.named(sqlStatement),
-        parameters: {
-          'content': question.content,
-          'order': question.ordinal,
-          'formId': question.formId,
-          'responseEnum': question.resFormat.widgetType,
-          'questionId': question.questionId,
-        },
-      );
-      if (result.isEmpty) {
-        throw Exception('Failed to update question.');
-      }
-    }  finally {
-      PostgresDB.closeConnection();
-    }
+  Future<void> updateQuestion(Question question) async 
+  {
+    // TODO: implement updateQuestion
+    var result = await Database.updateQuestion( question.content, 
+                                                question.ordinal,
+                                                question.formId,
+                                                question.resFormat.widgetType,
+                                                question.questionId);
+
+    if (result.isEmpty) { throw Exception('Failed to update question.'); }
   }
 
 /// called when saving the entire form
 /// 
 /// Removes all existing questions for a form, in order to re-add the newest ones (in [questions])
-Future<void> updateFormQuestions({required List<Question> questions, required String formId}) async {
-  try {
-    var db = await _connection; /// get the static connection
+Future<void> updateFormQuestions({required List<Question> questions, required String formId}) async 
+{
+  // Remove existing questions for the form
+  await Database.deleteFormById(formId);
 
-    // Remove existing questions for the form
-    String deleteSql = "DELETE FROM public.tbl_questions WHERE form_id = @formId";
-    await db.execute(deleteSql, parameters: {'formId': formId});
-
-    // Insert new questions for the form
-    String insertSql = "INSERT INTO public.tbl_questions (content, order_in_form, form_id, response_enum) VALUES ";
-    List<String> values = [];
-    for (var question in questions) {
-      values.add("('${question.content}', ${question.ordinal}, '$formId', ${question.resFormat.widgetType})");
-    }
-    insertSql += values.join(", ");
-    await db.execute(insertSql);
-  } finally {
-    PostgresDB.closeConnection();
+  List<String> values = [];
+  for (var question in questions) 
+  {
+    values.add("('${question.content}', ${question.ordinal}, '$formId', ${question.resFormat.widgetType})");
   }
+
+  // Insert new questions for the form
+  await Database.insertNewQuestions(values.join(", "));
 }
 }
 
+/// 
 Question _mapRowToQuestion(Map<String, dynamic> row) {
   return Question(
       questionId: row['question_id'],
@@ -172,4 +118,5 @@ Question _mapRowToQuestion(Map<String, dynamic> row) {
       resFormat: ResponseType(widgetType: row['response_enum']),
       linkedFileKey: row['linked_file_key'], // Assuming you store file references
     );
+
 }
