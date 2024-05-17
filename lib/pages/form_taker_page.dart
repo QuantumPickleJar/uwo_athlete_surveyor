550a885803a5ec5ed9e2966b6b233612c13e2337
QuantumPickleJar <vmorrilll@gmail.com>
2024-05-17 10:40:56 -0500

    refactor: Update FormTakerPage to handle navigation and user responses

---
+++ lib/pages/form_taker_page.dart
@@ -0,0 +1,85 @@
+import 'package:athlete_surveyor/widgets/form_progress_widget.dart';
+import 'package:flutter/material.dart';
+
+/// Page for students to take the form and submit their responses.
+class FormTakerPage extends StatefulWidget {
+  final List<String> questions;
+
+  const FormTakerPage({Key? key, required this.questions}) : super(key: key);
+
+  @override
+  _FormTakerPageState createState() => _FormTakerPageState();
+}
+
+class _FormTakerPageState extends State<FormTakerPage> {
+  int currentQuestionIndex = 0;
+  Set<int> answeredQuestions = {};
+
+  void answerCurrentQuestion() {
+    setState(() {
+      answeredQuestions.add(currentQuestionIndex);
+    });
+  }
+
+  void previousQuestion() {
+    setState(() {
+      if (currentQuestionIndex > 0) {
+        currentQuestionIndex--;
+      }
+    });
+  }
+
+  void nextQuestion() {
+    setState(() {
+      if (currentQuestionIndex < widget.questions.length - 1) {
+        currentQuestionIndex++;
+      }
+    });
+  }
+
+  void onQuestionSelected(int index) {
+    setState(() {
+      currentQuestionIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Take Form'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            FormProgressWidget(
+              questions: widget.questions,
+              answeredQuestions: answeredQuestions,
+              currentQuestionIndex: currentQuestionIndex,
+              onQuestionSelected: onQuestionSelected,
+            ),
+            const SizedBox(height: 8.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton.extended(
+                  onPressed: previousQuestion,
+                  label: const Text('<< Prev'),
+                ),
+                ElevatedButton(
+                  onPressed: answerCurrentQuestion,
+                  child: const Text('Answer Current Question'),
+                ),
+                FloatingActionButton.extended(
+                  onPressed: nextQuestion,
+                  label: const Text('Next >>'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/form_progress_widget.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+
+/// A stateful widget that represents the form progress widget.
+/// It displays a list of questions and a progress indicator showing the completion percentage.
+class FormProgressWidget extends StatefulWidget {
+  final List<String> questions;
+  final Set<int> answeredQuestions;
+  final int currentQuestionIndex;
+  final ValueChanged<int> onQuestionSelected;
+
+  const FormProgressWidget({
+    Key? key,
+    required this.questions,
+    required this.answeredQuestions,
+    required this.currentQuestionIndex,
+    required this.onQuestionSelected,
+  }) : super(key: key);
+
+  @override
+  _FormProgressWidgetState createState() => _FormProgressWidgetState();
+}
+
+class _FormProgressWidgetState extends State<FormProgressWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _buildQuestionList(),
+                const SizedBox(width: 16.0),
+                _buildProgressIndicator(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// Builds the list of questions.
+  Widget _buildQuestionList() {
+    return Expanded(
+      flex: 2,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 1,
+              blurRadius: 2,
+              offset: const Offset(2, 2), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Questions',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            const Divider(height: 1, thickness: 2),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(8.0),
+                itemCount: widget.questions.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4.0),
+                    child: ListTile(
+                      title: Text(
+                        widget.questions[index],
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: widget.currentQuestionIndex == index
+                              ? FontWeight.bold
+                              : FontWeight.normal,
+                          color: widget.answeredQuestions.contains(index)
+                              ? Colors.grey
+                              : Colors.black,
+                          decoration: widget.answeredQuestions.contains(index)
+                              ? TextDecoration.lineThrough
+                              : TextDecoration.none,
+                        ),
+                      ),
+                      tileColor: widget.currentQuestionIndex == index
+                          ? Colors.black12
+                          : Colors.transparent,
+                      onTap: () {
+                        widget.onQuestionSelected(index);
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// Builds the progress indicator.
+  Widget _buildProgressIndicator() {
+    return Expanded(
+      flex: 1,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              SizedBox(
+                width: 100,
+                height: 100,
+                child: CircularProgressIndicator(
+                  value: widget.answeredQuestions.length / widget.questions.length,
+                  strokeWidth: 10,
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '${((widget.answeredQuestions.length / widget.questions.length) * 100).toInt()}%',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Text(
+                    'Progress',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
