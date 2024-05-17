92eb08bea4732450ab31957f84157b7cd92850eb
QuantumPickleJar <vmorrilll@gmail.com>
2024-05-17 01:13:35 -0500

    refactor: Update FormTileWidget to navigate to FormTakerPage with necessary data

--- lib/widgets/form_tile_widget.dart
+++ lib/widgets/form_tile_widget.dart
@@ -1,41 +1,41 @@
-import 'package:athlete_surveyor/data_objects/logged_in_user.dart';
-import 'package:athlete_surveyor/models/forms/base_form.dart';
-import 'package:athlete_surveyor/pages/staff/form_builder_page.dart';
-import 'package:athlete_surveyor/resources/common_functions.dart';
-import 'package:flutter/material.dart';
-    
-class FormTileWidget extends StatelessWidget {
-  final GenericForm form;
-  final LoggedInUser currentUser;
+  import 'package:athlete_surveyor/data_objects/logged_in_user.dart';
+  import 'package:athlete_surveyor/models/forms/base_form.dart';
+  import 'package:athlete_surveyor/pages/staff/form_builder_page.dart';
+  import 'package:athlete_surveyor/resources/common_functions.dart';
+  import 'package:flutter/material.dart';
+      
+  class FormTileWidget extends StatelessWidget {
+    final GenericForm form;
+    final LoggedInUser currentUser;
 
-  const FormTileWidget({
-    super.key,
-    required this.form,
-    required this.currentUser,
-  });
+    const FormTileWidget({
+      super.key,
+      required this.form,
+      required this.currentUser,
+    });
 
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(form.formName),
-      onTap: () {
-        // Staff users should be taken to [FormBuilderPage]
-        if (currentUser.hasAdminPrivileges) {
-          navigateToPage(
-            context,
-            FormBuilderPage(formId: form.formId),
-          );
-        } else {
-          navigateToPage(
-            context,
-            FormTakerPage(
-              form.formName,
-              form.sport,
-              /// send w/e needed to track responses.  Probably a [StudentForm]?
-            ),
-          );
-        }
-      },
-    );
+    @override
+    Widget build(BuildContext context) {
+      return ListTile(
+        title: Text(form.formName),
+        onTap: () {
+          // Staff users should be taken to [FormBuilderPage]
+          if (currentUser.hasAdminPrivileges) {
+            navigateToPage(
+              context,
+              FormBuilderPage(formId: form.formId),
+            );
+          } else {
+            navigateToPage(
+              context,
+              FormTakerPage(
+                form.formName,
+                form.sport,
+                /// send w/e needed to track responses.  Probably a [StudentForm]?
+              ),
+            );
+          }
+        },
+      );
+    }
   }
-}
