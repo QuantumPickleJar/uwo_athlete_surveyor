bffe843b7ece936cd224f08a92830f2b0936e73a
Vincent Morrill <morriv92@uwosh.edu>
2024-05-18 18:03:27 -0500

    refactor: Update User class hierarchy and add Staff and Student subclasses

--- lib/data_objects/logged_in_user.dart
+++ lib/data_objects/logged_in_user.dart
@@ -17,3 +17,25 @@ class LoggedInUser
 
   LoggedInUser(this._uuid, this._isAdmin, this._isTempPassword, this._username, this._firstName, this._lastName);
 }
+
+/// Most basic user class, to represent a registered user
+abstract class User {
+  final String userId;
+  final String username;
+  final String firstName; 
+  final String lastName;
+
+  User({required this.userId, required this.username, required this.firstName, required this.lastName});
+}
+
+class Staff extends User {
+  Staff({required super.userId, required super.username, required super.firstName, required super.lastName});
+  
+  // Additional staff-specific methods here
+}
+
+class Student extends User {
+  Student({required super.userId, required super.username, required super.firstName, required super.lastName});
+  
+  // Additional staff-specific methods here
+}
