7d57afd65f2c3df8a14a95f62db8037d717ac9e2
Vincent Morrill <morriv92@uwosh.edu>
2024-05-17 19:22:42 -0500

    refactor: Update home page to use CreateFormButton widget for creating forms

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,19 @@
 /// 
 /// Authors: Josh, Vince, Amanda.
 /// Version:          0.0.1
-/// Remarks: ProxyProvider was extremely powerful to be working with starting out
 
-import 'dart:convert';
 import 'package:athlete_surveyor/data_objects/logged_in_user.dart';
+import 'package:athlete_surveyor/models/change_password_model.dart';
+import 'package:athlete_surveyor/models/create_user_model.dart';
 import 'package:athlete_surveyor/models/inbox_model.dart';
 import 'package:athlete_surveyor/models/login_model.dart';
 import 'package:athlete_surveyor/models/forms/previous_forms_model.dart';
-import 'package:athlete_surveyor/models/sport.dart';
 import 'package:athlete_surveyor/models/sport_selection_model.dart';
+import 'package:athlete_surveyor/pages/common/change_password_page.dart';
 import 'package:athlete_surveyor/pages/common/tabbed_main_page.dart';
 import 'package:athlete_surveyor/resources/common_functions.dart';
-import 'package:athlete_surveyor/services/db.dart';
+import 'package:athlete_surveyor/resources/common_widgets.dart';
+import 'package:athlete_surveyor/resources/constant_values.dart' as constants;
 import 'package:athlete_surveyor/services/forms/form_repository.dart';
 import 'package:athlete_surveyor/services/questions/question_repository.dart';
 import 'package:athlete_surveyor/services/sports/sports_repository.dart';
@@ -29,11 +30,11 @@ import 'models/student_model.dart';
 import 'package:flutter/foundation.dart' as foundation_dart;
 import 'services/forms/form_service.dart';
 
+/// Driver code.
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-late SportsRepository sportsRepository;
-  final sportsService = SportsService(null); // Temporarily set to null
+  late SportsRepository sportsRepository;
+  final sportsService = SportsService(null); // Temporarily set to null so the model can perform a lazy load
   final sportSelectionModel = SportSelectionModel(sportsService: sportsService);
 
   // Create the sports repository with the SportSelectionModel
@@ -45,14 +46,10 @@ late SportsRepository sportsRepository;
   // Load initial sports data
   final sports = await sportsService.loadSportsFromFile();
   sportSelectionModel.sports = sports;
-
-
-  /// Driver code.
   runApp(
-    /// Dependency injection time!
     MultiProvider(
       providers: [
-        /// Create SportSelectionModel next to load sport contents from assets
+       /// Create SportSelectionModel next to load sport contents from assets
         ChangeNotifierProvider<SportSelectionModel>(
           create: (context) => sportSelectionModel,
         ),
@@ -71,23 +68,25 @@ late SportsRepository sportsRepository;
         ProxyProvider3<SportsRepository, FormRepository, QuestionRepository, FormService>(
           update: (_, sportsRepo, formRepo, questionRepo, __) => FormService(formRepo, questionRepo),
         ),
-        
         ChangeNotifierProvider(create: (context) => LoginModel()),
         ChangeNotifierProvider(create: (context) => InboxModel()),
         ChangeNotifierProvider(create: (context) => StudentsModel()),
-        /// [ChangeNotifierProxyProvider] used to create and update the [AuthoredFormsModel]
-        /// based on the changes in the [FormService]. It listens to changes in the [FormService]
-        /// and updates the [AuthoredFormsModel] accordingly.
+        // ChangeNotifierProvider(create: (context) => AuthoredFormsModel()),
+
+        ChangeNotifierProvider(create: (context) => CreateUserModel()),
+        ChangeNotifierProvider(create: (context) => ChangePasswordModel()),
+        ///Listens to changes in the [FormService] and updates the [AuthoredFormsModel] accordingly.
         ChangeNotifierProxyProvider<FormService, AuthoredFormsModel>(
           create: (_) => AuthoredFormsModel(Provider.of<FormService>(_, listen: false)),
           update: (_, formService, previous) => previous!..updateFormService(formService),
         ),
-      ], 
+      ],
       child: MaterialApp(home: MainApp(LoginModel()))
     )
   );
 }
 
+/// Main
 class MainApp extends StatefulWidget
 {
   final LoginModel loginModel;
@@ -97,32 +96,47 @@ class MainApp extends StatefulWidget
   _MainAppState createState() => _MainAppState();
 }
 
+/// Main State
 class _MainAppState extends State<MainApp>
 {
+  final _formKey = GlobalKey<FormState>(); // So we can reference the Form where we need it.
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   String? username;
   String? password;
 
-  TextEditingController usernameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  final _formKey = GlobalKey<FormState>(); // so we can reference the Form where we need it
-
   /// Form validation; check entered username.
-  String? _validateUsername(String username)
-  {
-    return username.isNotEmpty ? null : 'Username field left blank, please enter username';
-  }
+  String? _validateUsername(String username) { return username.isNotEmpty ? null : constants.invalidUsernameString; }
 
   /// Form validation; check entered password.
-  String? _validatePassword(String password)
+  String? _validatePassword(String password) { return password.isNotEmpty ? null : constants.invalidPasswordString; }
+
+  /// Validate supplied login information against database.
+  void _validateLogin(BuildContext context) async
   {
-    return password.isNotEmpty ? null : 'Password field left blank, please enter password';
+    if(_formKey.currentState!.validate())
+    {
+      LoggedInUser? currentUser = await widget.loginModel.checkExistingPassword(usernameController.text, passwordController.text);
+ 
+      if(context.mounted && currentUser != null) 
+      {
+        if(currentUser.hasTempPassword) // If password is still the temporary password assigned at user creation, make user change it on first login.
+        {
+          navigateToPage(context, Consumer<ChangePasswordModel> (builder: (context, changePasswordModel, child) => ChangePasswordPage(changePasswordModel, currentUser: currentUser)));
+        }
+        else
+        {
+          navigateToPage(context, TabbedMainPage(currentUser: currentUser)); 
+        }
+      }
+    }
   }
 
   /// Used specifically to speed up student account login for testing purposes.
   void _buttonPressTestStudentLogin(BuildContext context) async
   {
-    usernameController.text = 'testStudent@uwosh.edu';
-    passwordController.text = '1A5qGrb6p4!%a4Iw';
+    usernameController.text = constants.testStudentUsername;
+    passwordController.text = constants.testStudentPassword;
 
     _validateLogin(context);
   }
@@ -130,19 +144,10 @@ class _MainAppState extends State<MainApp>
   /// Used specifically to speed up admin account login for testing purposes.
   void _buttonPressTestAdminLogin(BuildContext context) async
   {
-    usernameController.text = 'admin@uwosh.edu';
-    passwordController.text = 'newpasswordtest';
+    usernameController.text = constants.testAdminUsername;
+    passwordController.text = constants.testAdminPassword;
 
     _validateLogin(context);
-  }
-
-  /// Validate supplied login information against database.
-  void _validateLogin(BuildContext context) async
-  {
-    if(_formKey.currentState!.validate()) {
-      LoggedInUser? currentUser = await widget.loginModel.checkExistingPassword(usernameController.text, passwordController.text);
-      if(context.mounted && currentUser != null) { navigateToPage(context, TabbedMainPage(currentUser: currentUser)); }
-    }
   }
 
   @override
@@ -150,53 +155,41 @@ class _MainAppState extends State<MainApp>
   {
     return Scaffold(
       body: Container(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(constants.defaultEdgeInsetsPadding),
         alignment: Alignment.center,
         child: Form(
           key: _formKey,
-          child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Container(
                 width: 275,
                 height: 200,
                 alignment: Alignment.center,
                 decoration: const BoxDecoration(
-                  image: DecorationImage(image: NetworkImage('https://uwosh.edu/umc/wp-content/uploads/sites/18/2019/07/UWO_vertical_Oshkosh_4c.png'), fit: BoxFit.fill)
-                ),
-              ),
-              const Text('Be Better Initiative', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
-              TextFormField(
-                controller: usernameController,
-                validator: (username) => _validateUsername(username!),
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter Username')),
-              TextFormField(
-                controller: passwordController,
-                validator: (password) => _validatePassword(password!),
-                obscureText: true,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter Password')),
+                  image: DecorationImage(image: NetworkImage(constants.uwoWatermarkUrl), fit: BoxFit.fill))),
+              const Text(constants.pageTitle, style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+              defaultTextFormField(
+                controller: usernameController, 
+                validator: (username) => _validateUsername(username!), 
+                hintText: constants.loginUsernameFieldHint,
+                obscureText: false),
+              defaultTextFormField(
+                controller: passwordController, 
+                validator: (password) => _validatePassword(password!), 
+                hintText: constants.loginPasswordFieldHint,
+                obscureText: true),
               ElevatedButton(
                 onPressed: (){ _validateLogin(context); },
                 style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.yellow)), 
-                child: const SizedBox(child: Center(child: Text('Login', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))))),
-              Visibility(
-                visible: foundation_dart.kDebugMode, //only visible in debug mode
-                child: ElevatedButton(
-                  onPressed: (){ _buttonPressTestStudentLogin(context); },
-                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.yellow)), 
-                  child: const SizedBox(child: Center(child: Text('DEBUG: Login as Student', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))))),
-              ),
-              Visibility(
-                visible: foundation_dart.kDebugMode, //only visible in debug mode
-                child: ElevatedButton(
-                  onPressed: (){ _buttonPressTestAdminLogin(context); }, 
-                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.yellow)), 
-                  child: const SizedBox(child: Center(child: Text('DEBUG: Login as Staff (ADMIN)', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))))),
-              )
-            ]),
-        )));
+                child: const SizedBox(child: Center(child: Text(constants.loginButtonText, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))))),
+              toggleVisibleButton( // Only visible in debug mode
+                visibilityToggle: foundation_dart.kDebugMode, 
+                onPressed: (){ _buttonPressTestStudentLogin(context); }, 
+                buttonText: constants.testStudentLoginButtonText),
+              toggleVisibleButton( // Only visible in debug mode
+                visibilityToggle: foundation_dart.kDebugMode, 
+                onPressed: (){ _buttonPressTestAdminLogin(context); }, 
+                buttonText: constants.testAdminLoginButtonText)]))));
   }
 }
