77ede9fa4f5a835b695232b60b4ddc5f3225c626
Vincent Morrill <morriv92@uwosh.edu>
2024-05-20 15:14:06 -0500

    refactor: Update LoggedInUser class to extend User and remove unnecessary properties

--- lib/data_objects/logged_in_user.dart
+++ lib/data_objects/logged_in_user.dart
@@ -1,41 +1,23 @@
 // Data Object to represent the currently logged-in user and any information about them that may be needed during use of application.
-class LoggedInUser
+import 'package:athlete_surveyor/models/users/user_types.dart';
+
+class LoggedInUser extends User
 {
-  final String _uuid;
-  final bool _isAdmin;
-  final bool _isTempPassword;
-  final String _username;
-  final String _firstName;
-  final String _lastName;
+  // final String _uuid;
+  final bool isAdmin;
+  final bool isTempPassword;
 
-  String get userUuid => _uuid;
-  bool get hasAdminPrivileges => _isAdmin;
-  bool get hasTempPassword => _isTempPassword;
-  String get firstName => _firstName;
-  String get lastName => _lastName;
-  String get email => _username;
+  /// wraps the user in a model-friendly class that extends [User]
+  LoggedInUser({
+    required super.userId, 
+    required super.username, 
+    required super.firstName, 
+    required super.lastName, 
+    required this.isAdmin,
+    required this.isTempPassword
+  });
 
-  LoggedInUser(this._uuid, this._isAdmin, this._isTempPassword, this._username, this._firstName, this._lastName);
-}
+  bool get hasAdminPrivileges => isAdmin;
+  bool get hasTempPassword => isTempPassword;
 
-/// Most basic user class, to represent a registered user
-abstract class User {
-  final String userId;
-  final String username;
-  final String firstName; 
-  final String lastName;
-
-  User({required this.userId, required this.username, required this.firstName, required this.lastName});
-}
-
-class Staff extends User {
-  Staff({required super.userId, required super.username, required super.firstName, required super.lastName});
-  
-  // Additional staff-specific methods here
-}
-
-class Student extends User {
-  Student({required super.userId, required super.username, required super.firstName, required super.lastName});
-  
-  // Additional staff-specific methods here
 }
