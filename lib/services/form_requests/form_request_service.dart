c40e50fc228e97d8ba45b00bba8e0fa179779efa
Vincent Morrill <morriv92@uwosh.edu>
2024-05-26 13:44:10 -0500

    refactor: Add email sending functionality to FormRequestService

--- lib/services/form_requests/form_request_service.dart
+++ lib/services/form_requests/form_request_service.dart
@@ -59,8 +59,14 @@ class FormRequestService {
   Future<List<Future<GenericForm>>> getFormRequestsByUserId(String userId) async {
     try {
       var formRequests = await _formRequestRepository.getUserAuthoredFormRequests(userId);
+    
+      // return Future.wait(formRequests.map((request) async {
+      //   var form = await _formRepository.getFormById(request.formId);
+      //   return form!;
+      // }).toList());
+      
       return formRequests.map((request) async {
-        print('Retreived requests ${formRequests}');
+        print('[FormReqService] Processing FR {${request.requestId}, ${request.requestingUserId}}');
         
         var form = await _formRepository.getFormById(request.formId);
         return form!;
@@ -68,6 +74,75 @@ class FormRequestService {
     } catch (e) {
       print('Error fetching form requests for user with id $userId: $e');
       rethrow;
+    }
+  }
+
+
+  /// Create a FormRequest from an [Email] being sent out, based on the students 
+  /// listed as a recipient.
+  /// 
+  /// Since a form request may not necessarily be involved with the email, features
+  /// a call to [sendEmail]
+  Future<void> sendEmailWithFormRequest({
+    required String senderId, 
+    required List<String> recipientIds,
+    required String subject,
+    required String body,
+    String? formId,
+  }) async { 
+    try {
+      /// send the email 
+      await sendEmail(
+        senderId: senderId, 
+        recipientIds: recipientIds, 
+        subject: subject, 
+        body: body
+        );
+
+      /// if [formId] is present, we'll need a new form request
+      if (formId != null) {
+        await createFormRequest(formId, senderId, recipientIds);
+        /// TODO: might want to check for existing form requests matching [formId]
+      }
+    } catch (e) {
+      print('Error sending email w/ form request: $e');
+      rethrow;
+    } finally {
+      await PostgresDB.closeConnection();
+    }
+  }
+  
+  //// TODO: Might want to move this to an email service if things like read receipts are
+  /// desired
+  /// rudimentary email sending function
+  Future<void> sendEmail(
+    {required String senderId, 
+    required List<String> recipientIds,
+    required String subject,
+    required String body}) async {
+      try {
+        var db = await PostgresDB.getConnection();
+        await db.runTx((tx) async {
+        // Insert email into the inbox
+        for (String recipientId in recipientIds) {
+          String sqlSendEmail = """
+            INSERT INTO tbl_inbox (date_received, from_uuid, to_uuid, subject_line, body)
+            VALUES (@dateReceived, @fromUuid, @toUuid, @subjectLine, @body);
+          """;
+          await tx.execute(Sql.named(sqlSendEmail), parameters: {
+            'dateReceived': DateTime.now().toIso8601String(),
+            'fromUuid': senderId,
+            'toUuid': recipientId,
+            'subjectLine': subject,
+            'body': body,
+          });
+        }
+      });
+    } catch (e) {
+      print('Error sending email: $e');
+      rethrow;
+    } finally {
+      await PostgresDB.closeConnection();
     }
   }
 }
